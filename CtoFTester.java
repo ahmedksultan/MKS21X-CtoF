707014d0eadd10d67074ca70f94135768a645f1e
asultan00 <asultan00@stuy.edu>
2018-09-18 18:54:47 -0400

    added celsiusToFahrenheit function

---
+++ CtoFTester.java
@@ -0,0 +1,10 @@
+public class CtoFTester {
+     public static void main (String [] args) {
+          //final results go here
+          //celsiusToFahrenheit = 50.0
+          System.out.println(celsiusToFahrenheit(10.0));
+     }
+     public static double celsiusToFahrenheit(double c) {
+          return ((c * 9 / 5) + 32);
+     }
+}
